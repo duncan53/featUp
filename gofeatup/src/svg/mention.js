f36bf3f51463e021cb5725049458af1e27a184e3
IMAGE-ET <cedric_tavernier@image-et.fr>
2021-07-06 15:25:04 +0200

    Mentions légales

---
+++ gofeatup/src/svg/mention.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MentionSVG() {
+
+  return (
+    <svg width="34" height="24" viewBox="0 0 34 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 22H27M2 2H32H2ZM2 12H18.6667H2Z" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+
+  );
+}
+
+export default MentionSVG;
